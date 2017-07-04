2f7774fe7f692957a598c623d8a226456e8c9689
Ricardo Ferreira <ferreira.90.ricardo@gmail.com>
2017-07-05 00:11:01 +0100

    Interface Last Version

--- Web Application/Traker-app/src/app/elements/season-grid/season-grid.component.ts
+++ Web Application/Traker-app/src/app/elements/season-grid/season-grid.component.ts
@@ -1,6 +1,6 @@
 
 
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import 'rxjs/Rx'
 
 @Component({
@@ -10,12 +10,9 @@ import 'rxjs/Rx'
 })
 
 export class SeasonGridComponent {
-  num = 8;
-  range = (value) => {
-    let a = [];
-    for(let i = 0; i < value; ++i) {
-      a.push(i)
-    }
-    return a;
+  @Input()
+  media: Object = {};
+
+  constructor() {
   }
 }
