72d49922b5c76082b323c076f7824faa4ea4f8be
Nuno Fernandes <nunofernandes@guimaform.pt>
2017-07-04 22:37:14 +0100

    dabiushdoas

---
+++ Web Application/Traker-app/src/app/elements/user-watchlist/user-watchlist.component.ts
@@ -0,0 +1,12 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-watchlist',
+  templateUrl: './user-watchlist.component.html',
+  styleUrls: ['./user-watchlist.component.css']
+})
+
+export class UserWatchListComponent {
+}
+
